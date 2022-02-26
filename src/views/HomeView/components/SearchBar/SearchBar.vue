85c3219c5f74d6d421505731abce099c3e0d2685
sirirat-saemak <sirirat.saemak@allianz.com>
2022-02-26 15:29:21 +0700

    feat: adjust search bar and bind with state

--- src/views/HomeView/components/SearchBar/SearchBar.vue
+++ src/views/HomeView/components/SearchBar/SearchBar.vue
@@ -2,17 +2,35 @@
   <div>
     <v-subheader class="d-flex justify-space-between">
       Headline
-      <v-btn dark @click.stop="drawer = !drawer"> FITTER </v-btn>
+      <v-btn dark @click="openFilter"> FITTER </v-btn>
     </v-subheader>
-    <v-navigation-drawer v-model="drawer" absolute right temporary>
+    <v-navigation-drawer :value.sync="isOpenFilter" absolute right temporary>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Filter option </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
       <v-list dense>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
+        <v-list-item>
           <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-text-field
+              v-model="search.q"
+              counter
+              maxlength="50"
+              label="Keyword"
+            ></v-text-field>
           </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-autocomplete
+            v-model="search.source"
+            label="Source"
+            clearable
+          ></v-autocomplete>
+        </v-list-item>
+        <v-list-item class="d-flex justify-end">
+          <v-btn dark @click="findNews(search)"> FITTER </v-btn>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
@@ -20,16 +38,18 @@
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex';
+
 export default {
   name: 'SearchBar',
-  data() {
-    return {
-      drawer: null,
-      items: [
-        { title: 'Home', icon: 'mdi-view-dashboard' },
-        { title: 'About', icon: 'mdi-forum' },
-      ],
-    };
+  computed: {
+    ...mapState({
+      isOpenFilter: (state) => state.isOpenFilter,
+      search: (state) => state.search,
+    }),
+  },
+  methods: {
+    ...mapActions(['findNews', 'openFilter']),
   },
 };
 </script>
